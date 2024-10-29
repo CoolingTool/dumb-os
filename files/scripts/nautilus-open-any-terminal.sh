6b304ac495c9529d00645f7d99f1a09873e10d14
CoolingTool <62089272+CoolingTool@users.noreply.github.com>
2024-10-29 11:10:40 -0500

    nautilus python ext dependencies

--- files/scripts/nautilus-open-any-terminal.sh
+++ files/scripts/nautilus-open-any-terminal.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# https://aur.archlinux.org/packages/nautilus-open-any-terminal
+if ! rpm -q nautilus-python &> /dev/null; then
+  echo "Installing \"nautilus-python\" package, which is necessary for Brew to function"
+  rpm-ostree install nautilus-python
+fi
+if ! rpm -q gettext &> /dev/null; then
+  echo "Installing \"gettext\" package, which is necessary for Brew to function"
+  rpm-ostree install gettext
+fi
+if ! command -v make &> /dev/null; then
+  echo "Installing \"make\" package, which is necessary for nautilus-open-any-terminal to function"
+  rpm-ostree install make
+fi
+
+
 set -oue pipefail
 
 # https://github.com/Stunkymonkey/nautilus-open-any-terminal#from-source
