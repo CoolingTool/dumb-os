25f710b9af7527f5e17de27b06d3e8a9f8d0cf48
CoolingTool <62089272+CoolingTool@users.noreply.github.com>
2024-11-01 05:26:00 -0500

    5 am commit

--- files/scripts/omen-fan.sh
+++ files/scripts/omen-fan.sh
@@ -23,11 +23,22 @@ fi
 
 set -x
 
+# Installing acpi_ec
+
+git clone https://github.com/saidsay-so/acpi_ec && cd acpi_ec
+
+git apply $CONFIG_DIRECTORY/scripts/patches/acpi-no-mok.patch
+
+./install.sh
+
+# Installing omen-fan
+
 mkdir -p /usr/include && cd /usr/include
 git clone https://github.com/alou-S/omen-fan && cd omen-fan
 
-python3 -m venv env
-./env/bin/python3 -m pip install click tomlkit click-aliases
+git apply $CONFIG_DIRECTORY/scripts/patches/omen-fan-acpi.patch
+
+python3 -m venv env && ./env/bin/python3 -m pip install click tomlkit click-aliases
 
 sed -i 's/#!\/usr\/bin\/env /#!\/usr\/include\/omen-fan\/env\/bin\//' *.py
 
