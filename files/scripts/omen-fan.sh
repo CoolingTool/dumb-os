de106bb2c823ad17b6ae32a5fb077e7099bab6eb
CoolingTool <62089272+CoolingTool@users.noreply.github.com>
2024-10-31 22:38:37 -0500

    this still might not work

--- files/scripts/omen-fan.sh
+++ files/scripts/omen-fan.sh
@@ -3,29 +3,26 @@
 set -oue pipefail
 
 # https://wiki.archlinux.org/title/HP_Omen_16-c0140AX#Fan_control_Script
-# No install instructions, but that's fine, it's very trival to do so
+# No install instructions, but that's fine, it should be trival to do so
 #
 # Not for these people though https://redd.it/1e5bh6x/
 
-if ! rpm -q python3-tomlkit &> /dev/null; then
-  echo "Installing \"python3-tomlkit\" package, which is necessary for omen-fan to function"
-  rpm-ostree install python3-tomlkit
-fi
-
-# Bad idea
-if ! rpm -q python3-pip &> /dev/null; then
-  echo "Installing \"python3-pip\" package, which is necessary for omen-fan to function"
-  rpm-ostree install python3-pip
+if ! rpm -q python3 &> /dev/null; then
+  echo "Installing \"python3\" package, which is necessary for omen-fan to function"
+  rpm-ostree install python3
 fi
 
 set -x
 
-# Worse idea
-echo "Installing \"click-aliases\" via pip, which is necessary for omen-fan to function"
-rpm-ostree uninstall python3-click
-pip install click click-aliases --root-user-action=ignore
+mkdir -p /usr/include && cd /usr/include
+git clone https://github.com/alou-S/omen-fan && cd omen-fan
 
-git clone https://github.com/alou-S/omen-fan
+python3 -m venv env
+source env/bin/activate
+python3 -m pip install click tomlkit click-aliases
+deactivate
 
-install -T -m 755 omen-fan/omen-fan.py /usr/bin/omen-fan
-install -T -m 755 omen-fan/omen-fand.py /usr/bin/omen-fand
+sed -i 's/#!\/usr\/bin\/env python3/#!\/usr\/include\/omen-fan\/env\/bin\/python3' *.py
+
+install -T -m 755 omen-fan.py /usr/bin/omen-fan
+install -T -m 755 omen-fand.py /usr/bin/omen-fand
